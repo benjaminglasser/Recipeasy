682f39cde18d8ff2a15064a7a4901838a034b276
ben glasser <ben@fantasticvoyage.com>
2021-04-21 11:29:04 -0700

    added route for new recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recipeCtrl = require('../controllers/recipes');
 
 router.get('/', recipeCtrl.index);
+router.get('/new', recipeCtrl.new);
 
 module.exports = router;
